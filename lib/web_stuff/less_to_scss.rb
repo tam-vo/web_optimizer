2ea4e388001c0cb5ece29a8903be5ed8c635136c
Tam Vo <vo.mita.ov@gmail.com>
2014-02-22 12:18:37 +0700

    Fix less to scss

--- lib/web_stuff/less_to_scss.rb
+++ lib/web_stuff/less_to_scss.rb
@@ -4,13 +4,15 @@ module WebStuff
       raise "Invalid less file" unless File.extname(path) == ".less"
 
       content = File.read(path)
-      content.gsub!(/@/, "$").gsub!(/\.([\w\-]*)\s*\((.*)\)\s*\{/, "@mixin \1\(\2\)\n{")
-      content.gsub!(/\.([\w\-]*\(.*\)\s*;)/, "@include \1").gsub!(/~"(.*)"/, "\#{\"\1\"}")
-      content.gsub!("$import", "@import")
-      content.gsub!("$media", "@media")
-      content.gsub!("$keyframes", "@-moz-")
-      content.gsub!("$-o-", "@-o-")
-      content.gsub!("$-webkit-", "@-webkit-")
+      content.gsub!(/@\{?(\w+)\}?/, '$\1')
+      content.gsub!(/\.([\w\-]*)\s*\((.*)\)\s*\{/, '@mixin \1(\2){')
+      content.gsub!(/\.([\w\-]*\(.*\)\s*;)/, '@include \1')
+      content.gsub!(/~"(.*)"/, '\#{\"\1\"}')
+      content.gsub!(/\$import +["\'](.*?)\.less["\']/, '@import "\1.scss"')
+      content.gsub!('$media', '@media')
+      content.gsub!('$keyframes', '@-moz-')
+      content.gsub!('$-o-', '@-o-')
+      content.gsub!('$-webkit-', '@-webkit-')
 
       Common::Util.write_file(File.join(File.dirname(path), File.basename(path, ".*") + ".scss"), content)
     end
